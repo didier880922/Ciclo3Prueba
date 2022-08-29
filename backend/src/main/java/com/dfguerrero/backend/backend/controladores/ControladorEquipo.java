57f2f2f9cd7188838749466b4cc2c649dffef207
DIDIER FERNANDO GUERRERO SUMALAVE <67668036+didier880922@users.noreply.github.com>
2022-08-28 23:24:21 -0500

    Update ControladorEquipo.java

--- backend/src/main/java/com/dfguerrero/backend/backend/controladores/ControladorEquipo.java
+++ backend/src/main/java/com/dfguerrero/backend/backend/controladores/ControladorEquipo.java
@@ -2,7 +2,6 @@ package com.dfguerrero.backend.backend.controladores;
 
 import java.util.List;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
